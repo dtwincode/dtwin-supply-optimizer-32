3b8e8fd20ff8156ba03bc801e80bf5cbc467ee34
dtwincode <rdiab@dtwin.sa>
2025-03-26 08:44:19 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig(({ mode }) => ({
+  base: './',
   server: {
     host: true, // ✅ مطلوب أثناء التطوير
     port: 8080,
